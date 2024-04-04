9e6d24a877399b73bb3d9e425c1ae93e7a6f221c
ryosuke-kubota <kubota.miraidenshi@gmail.com>
2024-04-05 02:07:01 +0900

    Games Image slider

--- src/components/Games/index.tsx
+++ src/components/Games/index.tsx
@@ -2,20 +2,21 @@ import './style.scss';
 import RighteousText from '../Headings/RighteousText';
 import Image from 'next/image';
 import BorderButton from '../Buttons/BorderButton';
+import ImageSlider from '../Slider/ImageSlider';
 
-export default function Solutions() {
+export default function Games() {
   return (
     <section id="games">
       <div className="inner text-center py-20 px-4">
         <RighteousText tag="h2" className="text-5xl font-bold mb-12">
           QB GAMES
         </RighteousText>
-        <p className="mb-12">
+        <p className="mb-16">
           テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテ
           <br />
           キストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
         </p>
-        <div className="game-list flex items-center justify-center gap-5 gap-y-12">
+        <div className="game-list flex items-center justify-center gap-5 gap-y-12 mb-16">
           <div className="game w-1/4 flex flex-col items-center justify-center">
             <Image
               src={'/assets/images/games-img01.png'}
@@ -34,6 +35,9 @@ export default function Solutions() {
               height={'100'}
               alt={'KISARAGI'}
             ></Image>
+            <RighteousText tag="div" className="game-label">
+              QB ORIGINAL
+            </RighteousText>
           </div>
           <div className="game w-1/4 flex flex-col items-center justify-center">
             <Image
@@ -42,6 +46,9 @@ export default function Solutions() {
               height={'100'}
               alt={'KISARAGI'}
             ></Image>
+            <RighteousText tag="div" className="game-label">
+              QB ORIGINAL
+            </RighteousText>
           </div>
           <div className="game w-1/4 flex flex-col items-center justify-center">
             <Image
@@ -50,8 +57,13 @@ export default function Solutions() {
               height={'100'}
               alt={'KISARAGI'}
             ></Image>
+            <RighteousText tag="div" className="game-label">
+              QB ORIGINAL
+            </RighteousText>
           </div>
         </div>
+
+        <ImageSlider />
       </div>
     </section>
   );
---
+++ src/components/Slider/ImageSlider/index.tsx
@@ -0,0 +1,65 @@
+import Image from 'next/image';
+
+// オプションをインポートする
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './style.scss';
+
+// publicフォルダの画像
+const images = [
+  '/assets/images/slider-img_normal.jpg',
+  '/assets/images/slider-img_normal.jpg',
+  '/assets/images/slider-img_normal.jpg',
+  '/assets/images/slider-img_normal.jpg',
+];
+
+export default function ImageSlider() {
+  return (
+    <div className="image-slider">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={1} // ハイドレーションエラー対策
+        spaceBetween={0}
+        loop={true} // スライドをループさせる
+        speed={1000} // スライドが切り替わる時の速度
+        centeredSlides={true}
+        loopAdditionalSlides={1}
+        watchSlidesProgress={true}
+        // autoplay={{
+        //   delay: 4000,
+        //   disableOnInteraction: false,
+        // }}
+        pagination={{
+          el: '.swiper-pagination',
+        }} // ページネーション, クリックで対象のスライドに切り替わる
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }}
+        className={'swiper h-fit'}
+      >
+        {images.map((src: string, index: number) => (
+          <SwiperSlide tag={'a'} key={index} className="swiper-slide">
+            <article className="slide">
+              <Image
+                src={src}
+                width={1920}
+                height={1440}
+                alt="Slider Image"
+                // objectFit={'cover'}
+                // sizes="(min-width: 1024px) 100vw, 60vw"
+                // className={styles.slideImage}
+              />
+            </article>
+          </SwiperSlide>
+        ))}
+        <div className="swiper-pagination"></div>
+        <div className="swiper-button-prev"></div>
+        <div className="swiper-button-next"></div>
+      </Swiper>
+    </div>
+  );
+}
