0f0c65b8187905a493540bd27e9e2cbaadf2724f
ryosuke-kubota <kubota.miraidenshi@gmail.com>
2024-04-13 14:24:38 +0900

    ranking section animation

--- src/components/Ranking/index.tsx
+++ src/components/Ranking/index.tsx
@@ -1,18 +1,58 @@
 import './style.scss';
 import RighteousText from '../Headings/RighteousText';
-import Image from 'next/image';
-import BorderButton from '../Buttons/BorderButton';
-import ImageSlider from '../Slider/ImageSlider';
 import TopGamesTable from './TopGamesTable';
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 export default function Ranking() {
+  const rankingRef = useRef<HTMLDivElement>(null);
+  const rankingTitleRef = useRef<HTMLDivElement>(null);
+  const rankingTextRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: rankingRef.current,
+        start: 'top-=100 center',
+        once: true,
+        markers: false,
+      },
+    });
+
+    tl.from(
+      rankingTitleRef.current,
+      {
+        opacity: 0,
+        y: 50,
+        duration: 0.5,
+        delay: 0.1,
+        ease: 'power2.out',
+      },
+      'same'
+    ).from(
+      rankingTextRef.current,
+      {
+        opacity: 0,
+        y: 50,
+        duration: 0.5,
+        delay: 0.2,
+        ease: 'power2.out',
+      },
+      'same'
+    );
+  }, [rankingRef, rankingTitleRef, rankingTextRef]);
   return (
-    <section id="ranking">
+    <section id="ranking" ref={rankingRef}>
       <div className="inner text-center py-20 px-12">
-        <RighteousText tag="h2" className="text-5xl font-bold mb-12">
+        <RighteousText
+          tag="h2"
+          className="text-5xl font-bold mb-12"
+          ref={rankingTitleRef}
+        >
           Top Blockchain Games
         </RighteousText>
-        <p className="mb-16">
+        <p className="mb-16" ref={rankingTextRef}>
           テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテ
           <br />
           キストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
